d09100c5f1342cb0298337973082b1a985612cb2
Christian Franco <136227820+Chry55@users.noreply.github.com>
2024-06-13 18:28:36 +0200

    Add files via upload

--- Windows debloat script.ps1
+++ Windows debloat script.ps1
@@ -168,113 +168,94 @@ If (!(Test-Path "HKCU:\System\GameConfigStore")) {
                 New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Force
             }
             Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Name "AllowGameDVR" -Type DWord -Value 0
-			
-$services = @(
-                "ALG"                                          # Application Layer Gateway Service(Provides support for 3rd party protocol plug-ins for Internet Connection Sharing)
-                "AJRouter"                                     # Needed for AllJoyn Router Service
-                "BcastDVRUserService_48486de"                  # GameDVR and Broadcast is used for Game Recordings and Live Broadcasts
-                #"BDESVC"                                      # Bitlocker Drive Encryption Service
-                #"BFE"                                         # Base Filtering Engine (Manages Firewall and Internet Protocol security)
-                #"BluetoothUserService_48486de"                # Bluetooth user service supports proper functionality of Bluetooth features relevant to each user session.
-                #"BrokerInfrastructure"                        # Windows Infrastructure Service (Controls which background tasks can run on the system)
-                "Browser"                                      # Let users browse and locate shared resources in neighboring computers
-                "BthAvctpSvc"                                  # AVCTP service (needed for Bluetooth Audio Devices or Wireless Headphones)
-                "CaptureService_48486de"                       # Optional screen capture functionality for applications that call the Windows.Graphics.Capture API.
-                "cbdhsvc_48486de"                              # Clipboard Service
-                "diagnosticshub.standardcollector.service"     # Microsoft (R) Diagnostics Hub Standard Collector Service
-                "DiagTrack"                                    # Diagnostics Tracking Service
-                "dmwappushservice"                             # WAP Push Message Routing Service
-                "DPS"                                          # Diagnostic Policy Service (Detects and Troubleshoots Potential Problems)
-                "edgeupdate"                                   # Edge Update Service
-                "edgeupdatem"                                  # Another Update Service
-                #"EntAppSvc"                                    # Enterprise Application Management.
-                "Fax"                                          # Fax Service
-                "fhsvc"                                        # Fax History
-                "FontCache"                                    # Windows font cache
-                #"FrameServer"                                 # Windows Camera Frame Server (Allows multiple clients to access video frames from camera devices)
-                "gupdate"                                      # Google Update
-                "gupdatem"                                     # Another Google Update Service
-                #"iphlpsvc"                                     # ipv6(Most websites use ipv4 instead) - Needed for Xbox Live
-                "lfsvc"                                        # Geolocation Service
-                #"LicenseManager"                              # Disable LicenseManager (Windows Store may not work properly)
-                "lmhosts"                                      # TCP/IP NetBIOS Helper
-                "MapsBroker"                                   # Downloaded Maps Manager
-                "MicrosoftEdgeElevationService"                # Another Edge Update Service
-                "MSDTC"                                        # Distributed Transaction Coordinator
-                "NahimicService"                               # Nahimic Service
-                #"ndu"                                          # Windows Network Data Usage Monitor (Disabling Breaks Task Manager Per-Process Network Monitoring)
-                "NetTcpPortSharing"                            # Net.Tcp Port Sharing Service
-                "PcaSvc"                                       # Program Compatibility Assistant Service
-                "PerfHost"                                     # Remote users and 64-bit processes to query performance.
-                "PhoneSvc"                                     # Phone Service(Manages the telephony state on the device)
-                #"PNRPsvc"                                     # Peer Name Resolution Protocol (Some peer-to-peer and collaborative applications, such as Remote Assistance, may not function, Discord will still work)
-                #"p2psvc"                                      # Peer Name Resolution Protocol(Enables multi-party communication using Peer-to-Peer Grouping.  If disabled, some applications, such as HomeGroup, may not function. Discord will still work)iscord will still work)
-                #"p2pimsvc"                                    # Peer Networking Identity Manager (Peer-to-Peer Grouping services may not function, and some applications, such as HomeGroup and Remote Assistance, may not function correctly. Discord will still work)
-                "PrintNotify"                                  # Windows printer notifications and extentions
-                "QWAVE"                                        # Quality Windows Audio Video Experience (audio and video might sound worse)
-                "RemoteAccess"                                 # Routing and Remote Access
-                "RemoteRegistry"                               # Remote Registry
-                "RetailDemo"                                   # Demo Mode for Store Display
-                "RtkBtManServ"                                 # Realtek Bluetooth Device Manager Service
-                "SCardSvr"                                     # Windows Smart Card Service
-                "seclogon"                                     # Secondary Logon (Disables other credentials only password will work)
-                "SEMgrSvc"                                     # Payments and NFC/SE Manager (Manages payments and Near Field Communication (NFC) based secure elements)
-                "SharedAccess"                                 # Internet Connection Sharing (ICS)
-                #"Spooler"                                     # Printing
-                "stisvc"                                       # Windows Image Acquisition (WIA)
-                #"StorSvc"                                     # StorSvc (usb external hard drive will not be reconized by windows)
-                "SysMain"                                      # Analyses System Usage and Improves Performance
-                "TrkWks"                                       # Distributed Link Tracking Client
-                #"WbioSrvc"                                    # Windows Biometric Service (required for Fingerprint reader / facial detection)
-                "WerSvc"                                       # Windows error reporting
-                "wisvc"                                        # Windows Insider program(Windows Insider will not work if Disabled)
-                #"WlanSvc"                                     # WLAN AutoConfig
-                "WMPNetworkSvc"                                # Windows Media Player Network Sharing Service
-                "WpcMonSvc"                                    # Parental Controls
-                "WPDBusEnum"                                   # Portable Device Enumerator Service
-                "WpnService"                                   # WpnService (Push Notifications may not work)
-                #"wscsvc"                                      # Windows Security Center Service
-                "WSearch"                                      # Windows Search
-                "XblAuthManager"                               # Xbox Live Auth Manager (Disabling Breaks Xbox Live Games)
-                "XblGameSave"                                  # Xbox Live Game Save Service (Disabling Breaks Xbox Live Games)
-                "XboxNetApiSvc"                                # Xbox Live Networking Service (Disabling Breaks Xbox Live Games)
-                "XboxGipSvc"                                   # Xbox Accessory Management Service
-                # Hp services
-                "HPAppHelperCap"
-                "HPDiagsCap"
-                "HPNetworkCap"
-                "HPSysInfoCap"
-                "HpTouchpointAnalyticsService"
-                # Hyper-V services
-                "HvHost"
-                "vmicguestinterface"
-                "vmicheartbeat"
-                "vmickvpexchange"
-                "vmicrdv"
-                "vmicshutdown"
-                "vmictimesync"
-                "vmicvmsession"
-                # Services that cannot be disabled
-                #"WdNisSvc"
-            )
-        
-            foreach ($service in $services) {
-                # -ErrorAction SilentlyContinue is so it doesn't write an error to stdout if a service doesn't exist
-        
-                Write-Host "Setting $service StartupType to Manual"
-                Get-Service -Name $service -ErrorAction SilentlyContinue | Set-Service -StartupType Manual -ErrorAction SilentlyContinue
-            }
+
+$servicesToExclude = @(
+			"AudioSrv",
+			"AudioEndpointBuilder",
+			"BFE",
+			"BITS",
+			"BrokerInfrastructure",
+			"CDPSvc",
+			"CDPUserSvc_dc2a4",
+			"CoreMessagingRegistrar",
+			"CryptSvc",
+			"DPS",
+			"DcomLaunch",
+			"Dhcp",
+			"DispBrokerDesktopSvc",
+			"Dnscache",
+			"DoSvc",
+			"DusmSvc",
+			"EventLog",
+			"EventSystem",
+			"FontCache",
+			"LSM",
+			"LanmanServer",
+			"LanmanWorkstation",
+			"MapsBroker",
+			"MpsSvc",
+			"OneSyncSvc_dc2a4",
+			"Power",
+			"ProfSvc",
+			"RpcEptMapper",
+			"RpcSs",
+			"SCardSvr",
+			"SENS",
+			"SamSs",
+			"Schedule",
+			"SgrmBroker",
+			"ShellHWDetection",
+			"Spooler",
+			"SysMain",
+			"SystemEventsBroker",
+			"TextInputManagementService",
+			"Themes",
+			"TrkWks",
+			"UserManager",
+			"VGAuthService",
+			"VMTools",
+			"WSearch",
+			"Wcmsvc",
+			"WinDefend",
+			"Winmgmt",
+			"WlanSvc",
+			"WpnService",
+			"WpnUserService_dc2a4",
+			"cbdhsvc_dc2a4",
+			"edgeupdate",
+			"gpsvc",
+			"iphlpsvc",
+			"mpssvc",
+			"nsi",
+			"sppsvc",
+			"tiledatamodelsvc",
+			"vm3dservice",
+			"webthreatdefusersvc_dc2a4",
+			"wscsvc"
+)	
+	
+	$runningServices = Get-Service | Where-Object { $servicesToExclude -notcontains $_.Name }
+	foreach($service in $runningServices)
+	{
+Stop-Service -Name $service.Name -PassThru
+		Set-Service $service.Name -StartupType Manual
+		"Stopping service $($service.Name)" | Out-File -FilePath c:\windows\LogFirstRun.txt -Append -NoClobber
+}
 
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableActivityFeed" -Type DWord -Value 0
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "PublishUserActivities" -Type DWord -Value 0
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "UploadUserActivities" -Type DWord -Value 0
 
 reg.exe add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v "DisableSearchBoxSuggestions" /t REG_DWORD /d 1 /f
+reg.exe add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v "DisableNotificationCenter" /t REG_DWORD /d 1 /f
 reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete" /f /ve
 reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoComplete" /v "AutoSuggest" /t REG_SZ /d "No" /f
 reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
 dism /Online /Set-ReservedStorageState /State:Disabled
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "SubmitSamplesConsent" /t REG_DWORD /d "2" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "SpynetReporting" /t REG_DWORD /d "0" /f
 
 $confirmation = Read-Host "do you want to disable hibernation? (not recommended for laptop users) y/n"
 if ($confirmation -eq 'y') {
