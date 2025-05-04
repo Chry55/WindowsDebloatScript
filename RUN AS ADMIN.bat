20f4a1b3448b94afc6e3ca112eabb056bde5b0f2
Christian Franco <136227820+Chry55@users.noreply.github.com>
2025-05-04 12:38:51 +0200

    Update RUN AS ADMIN.bat

--- RUN AS ADMIN.bat
+++ RUN AS ADMIN.bat
@@ -1,3 +1,5 @@
 @echo off
+cd C:/WindowsDebloatScript/
+OOSU10.exe ooshutup10.cfg /quiet
 set "psScript=%~dp0Windows debloat script.ps1"
 powershell -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File ""%psScript%""' -Verb RunAs}";
